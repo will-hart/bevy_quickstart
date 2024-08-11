8937eb0c8dad002c28729d05076faf11f5d442f3
William Hart <hart.wl@gmail.com>
2024-08-12 08:20:40 +1000

    Add orthogonal menu state and settings menu

--- src/screens/pause.rs
+++ src/screens/pause.rs
@@ -7,6 +7,7 @@ use crate::theme::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_sub_state::<IsPaused>();
+    app.add_sub_state::<PauseState>();
 
     app.register_type::<PauseMenu>();
 
@@ -16,8 +17,11 @@ pub(super) fn plugin(app: &mut App) {
             .run_if(in_state(Screen::Playing).and_then(input_just_pressed(KeyCode::Escape))),
     );
 
-    app.add_systems(OnEnter(IsPaused::Paused), show_pause_menu);
-    app.add_systems(OnExit(IsPaused::Paused), hide_pause_menu);
+    app.add_systems(OnEnter(PauseState::PauseMenu), spawn_pause_menu);
+    app.add_systems(OnExit(PauseState::PauseMenu), despawn_pause_menus);
+
+    app.add_systems(OnEnter(PauseState::SettingsMenu), spawn_settings_menu);
+    app.add_systems(OnExit(PauseState::SettingsMenu), despawn_pause_menus);
 }
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, SubStates)]
@@ -29,20 +33,39 @@ pub enum IsPaused {
     Paused,
 }
 
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, SubStates)]
+/// This is used to determine what state the pause menu is in.
+#[source(IsPaused = IsPaused::Paused)]
+pub enum PauseState {
+    #[default]
+    PauseMenu,
+    SettingsMenu,
+}
+
 #[derive(Component, Reflect)]
 #[reflect(Component)]
 struct PauseMenu;
 
-fn toggle_pause(current_state: Res<State<IsPaused>>, mut next_state: ResMut<NextState<IsPaused>>) {
-    next_state.set(match current_state.get() {
-        IsPaused::Running => IsPaused::Paused,
-        IsPaused::Paused => IsPaused::Running,
-    })
+fn toggle_pause(
+    current_pause_state: Option<Res<State<PauseState>>>,
+    mut next_is_paused: ResMut<NextState<IsPaused>>,
+    mut next_state: ResMut<NextState<PauseState>>,
+) {
+    match current_pause_state {
+        Some(pause_state) => match pause_state.get() {
+            PauseState::PauseMenu => next_is_paused.set(IsPaused::Running),
+            PauseState::SettingsMenu => next_state.set(PauseState::PauseMenu),
+        },
+        None => {
+            // We aren't currently paused as the resource doesn't exist.
+            next_is_paused.set(IsPaused::Paused);
+        }
+    }
 }
 
-const PAUSE_MENU_BACKGROUND_COLOR: Color = Color::srgb(0.157, 0.157, 0.157);
+const PAUSE_MENU_BACKGROUND_COLOR: Color = Color::srgb(0.3, 0.3, 0.3);
 
-fn show_pause_menu(mut commands: Commands) {
+fn spawn_pause_menu(mut commands: Commands) {
     commands
         .ui_root()
         .insert(PauseMenu)
@@ -51,7 +74,7 @@ fn show_pause_menu(mut commands: Commands) {
                 .spawn(NodeBundle {
                     style: Style {
                         width: Val::Px(500.),
-                        height: Val::Px(150.),
+                        height: Val::Px(300.),
                         padding: UiRect::all(Val::Px(10.)),
                         flex_direction: FlexDirection::Column,
                         align_items: AlignItems::Center,
@@ -62,22 +85,62 @@ fn show_pause_menu(mut commands: Commands) {
                 })
                 .with_children(|children| {
                     children.header("Game Paused");
-                    children.button("Menu").observe(return_to_title_screen);
-                    children.button("Resume").observe(unpause_game);
+                    children.button("Settings").observe(trigger_settings_menu);
+                    children.button("Resume").observe(trigger_unpause_game);
+                    children.button("Exit").observe(trigger_return_to_title);
                 });
         });
 }
 
-fn hide_pause_menu(mut commands: Commands, pause_menu_query: Query<Entity, With<PauseMenu>>) {
+fn despawn_pause_menus(mut commands: Commands, pause_menu_query: Query<Entity, With<PauseMenu>>) {
     for menu in &pause_menu_query {
         commands.entity(menu).despawn_recursive();
     }
 }
 
-fn return_to_title_screen(_trigger: Trigger<OnPress>, mut next_screen: ResMut<NextState<Screen>>) {
+fn spawn_settings_menu(mut commands: Commands) {
+    commands
+        .ui_root()
+        .insert(PauseMenu)
+        .with_children(|children| {
+            children
+                .spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Px(500.),
+                        height: Val::Px(300.),
+                        padding: UiRect::all(Val::Px(10.)),
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: BackgroundColor(PAUSE_MENU_BACKGROUND_COLOR),
+                    ..default()
+                })
+                .with_children(|children| {
+                    children.header("Settings");
+                    children.button("Back").observe(trigger_pause_menu);
+                });
+        });
+}
+
+fn trigger_return_to_title(_trigger: Trigger<OnPress>, mut next_screen: ResMut<NextState<Screen>>) {
     next_screen.set(Screen::Title);
 }
 
-fn unpause_game(_trigger: Trigger<OnPress>, mut next_state: ResMut<NextState<IsPaused>>) {
-    next_state.set(IsPaused::Running);
+fn trigger_unpause_game(
+    _trigger: Trigger<OnPress>,
+    mut next_is_paused: ResMut<NextState<IsPaused>>,
+) {
+    next_is_paused.set(IsPaused::Running);
+}
+
+fn trigger_settings_menu(
+    _trigger: Trigger<OnPress>,
+    mut next_state: ResMut<NextState<PauseState>>,
+) {
+    next_state.set(PauseState::SettingsMenu);
+}
+
+fn trigger_pause_menu(_trigger: Trigger<OnPress>, mut next_state: ResMut<NextState<PauseState>>) {
+    next_state.set(PauseState::PauseMenu);
 }
